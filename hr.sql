deb8ad2dff93d2508460bcfda87b2d070807eeda
faizputra <69647424+faizput@users.noreply.github.com>
2023-12-21 17:29:45 +0800

    Data Cleaning

---
+++ hr.sql
@@ -0,0 +1,70 @@
+CREATE DATABASE projects;
+
+USE projects;
+
+SELECT * FROM hr;
+
+ALTER TABLE hr
+CHANGE COLUMN ï»¿id emp_id VARCHAR(20) NULL;
+
+DESCRIBE hr;
+
+SELECT birthdate FROM hr;
+
+SET sql_safe_updates = 0;
+
+UPDATE hr
+SET birthdate = CASE
+	WHEN birthdate LIKE '%/%' THEN date_format(str_to_date(birthdate, '%m/%d/%Y'), '%Y-%m-%d')
+    WHEN birthdate LIKE '%-%' THEN date_format(str_to_date(birthdate, '%m-%d-%Y'), '%Y-%m-%d')
+    ELSE NULL
+END;
+
+ALTER TABLE hr
+MODIFY COLUMN birthdate DATE;
+
+UPDATE hr
+SET hire_date = CASE
+	WHEN hire_date LIKE '%/%' THEN date_format(str_to_date(hire_date, '%m/%d/%Y'), '%Y-%m-%d')
+    WHEN hire_date LIKE '%-%' THEN date_format(str_to_date(hire_date, '%m-%d-%Y'), '%Y-%m-%d')
+    ELSE NULL
+END;
+
+ALTER TABLE hr
+MODIFY COLUMN hire_date DATE;
+
+UPDATE hr
+SET termdate = COALESCE(STR_TO_DATE(NULLIF(termdate, ''), '%Y-%m-%d %H:%i:%s UTC'), '0000-00-00 00:00:00')
+WHERE termdate IS NOT NULL AND termdate != '';
+
+SELECT termdate FROM hr;
+
+-- Update existing data, replacing empty strings with NULL
+UPDATE hr
+          SET TERMDATE=cast(to_timestamp_tz(TERMDATE, 'yyyy-mm-dd hh24:mi:ss "UTC"') 
+          as date) 
+          WHERE TERMDATE IS NOT NULL;
+
+-- Modify the column to DATE type
+ALTER TABLE hr
+MODIFY COLUMN termdate DATE;
+
+ALTER TABLE hr ADD COLUMN age INT;
+
+UPDATE hr
+SET age = timestampdiff(YEAR, birthdate, CURDATE());
+
+SELECT 
+	min(age) AS youngest,
+    max(age) AS oldest
+FROM hr;
+
+SELECT count(*) FROM hr WHERE age < 18;
+
+SELECT COUNT(*) FROM hr WHERE termdate > CURDATE();
+
+SELECT COUNT(*)
+FROM hr
+WHERE termdate = '0000-00-00';
+
+SELECT location FROM hr;
